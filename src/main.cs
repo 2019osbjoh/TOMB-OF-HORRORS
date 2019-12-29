9ab846f3b539a66a5c746a0cd4cbc758ef4718f3
2019osbjoh <john.osborne151@gmail.com>
2019-12-29 10:31:56 +0000

    ~

---
+++ src/main.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TombOfHorrors {
+    class Program {
+
+        static void Main(string[] args) {
+            gameTitle();
+            first();
+
+            }
+
+        public static void gameTitle() {
+            Console.WriteLine(@"
+             _____ ___  __  __ ____             ___  _____           _   _  ___  ____  ____   ___  ____  ____  
+            |_   _/ _ \|  \/  | __ )           / _ \|  ___|         | | | |/ _ \|  _ \|  _ \ / _ \|  _ \/ ___| 
+              | || | | | |\/| |  _ \   _____  | | | | |_     _____  | |_| | | | | |_) | |_) | | | | |_) \___ \ 
+              | || |_| | |  | | |_) | |_____| | |_| |  _|   |_____| |  _  | |_| |  _ <|  _ <| |_| |  _ < ___) |
+              |_| \___/|_|  |_|____/           \___/|_|             |_| |_|\___/|_| \_\_| \_\\___/|_| \_\____/ 
+              
+              ");
+        
+        }
+
+        public static void first() {
+            
+        }
+
+        public static void second() {
+            
+        }
+        
+        public static void third() {
+            
+        }
+
+        public static void final() {
+            
+        }
+
+        public static void gameOver() {
+            
+        }
+
+        public static void gameWin() {
+            
+        }
+    }
+}
